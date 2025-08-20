c5dfa930a12812155f9aecbc266bf58a4584a58f
BiancaSantana81 <biancasantana81@hotmail.com>
2025-08-20 20:58:09 -0300

    [refactor] modified expects in unit and integration

--- tests/integration/getAllProducts.spec.ts
+++ tests/integration/getAllProducts.spec.ts
@@ -7,13 +7,16 @@ describe("Products API Integration ", () => {
     const response = await request(app).get("/api/products");
 
     expect(response.status).toBe(200);
-    expect(response.body.data).toEqual([
-      { "id": "1", "name": "Notebook Gamer Pro", "price": 7500 },
-      { "id": "2", "name": "Mouse Sem Fio Ultra-leve", "price": 350 },
-      { "id": "3", "name": "Teclado Mecânico RGB", "price": 550 },
-      { "id": "4", "name": "Monitor 4K 27\"", "price": 2500 },
-      { "id": "5", "name": "Headset 7.1 Surround", "price": 600 },
-      { "id": "6", "name": "Webcam Full HD", "price": 400 },
-      { "id": "7", "name": "SSD NVMe 1TB", "price": 800 }])
+    expect(response.body).toMatchObject({
+      data: [
+        { id: "1", name: "Notebook Gamer Pro", price: 7500 },
+        { id: "2", name: "Mouse Sem Fio Ultra-leve", price: 350 },
+        { id: "3", name: "Teclado Mecânico RGB", price: 550 },
+        { id: "4", name: "Monitor 4K 27\"", price: 2500 },
+        { id: "5", name: "Headset 7.1 Surround", price: 600 },
+        { id: "6", name: "Webcam Full HD", price: 400 },
+        { id: "7", name: "SSD NVMe 1TB", price: 800 }
+      ]
+    });
   });
 });
--- tests/unit/productApi.unit.spec.ts
+++ tests/unit/productApi.unit.spec.ts
@@ -5,14 +5,29 @@ describe("Products API", () => {
     it("deve retornar a lista de produtos", async () => {
         nock("https://pretalab-api-439254010866.us-central1.run.app")
             .get("/products")
-            .reply(200, [{
-                id: 1, name: "Notebook Gamer Pro"
-            }]);
+            .reply(200, {
+                data: [
+                    { id: "1", name: "Notebook Gamer Pro", price: 7500 },
+                    { id: "2", name: "Mouse Sem Fio Ultra-leve", price: 350 },
+                    { id: "3", name: "Teclado Mecânico RGB", price: 550 },
+                    { id: "4", name: "Monitor 4K 27\"", price: 2500 },
+                    { id: "5", name: "Headset 7.1 Surround", price: 600 },
+                    { id: "6", name: "Webcam Full HD", price: 400 },
+                    { id: "7", name: "SSD NVMe 1TB", price: 800 }
+                ]
+            });
 
         const products = await getProducts();
 
-        expect(products).toEqual([
-            { id: 1, name: "Notebook Gamer Pro" }
+        expect(products.data).toEqual([
+            { id: "1", name: "Notebook Gamer Pro", price: 7500 },
+            { id: "2", name: "Mouse Sem Fio Ultra-leve", price: 350 },
+            { id: "3", name: "Teclado Mecânico RGB", price: 550 },
+            { id: "4", name: "Monitor 4K 27\"", price: 2500 },
+            { id: "5", name: "Headset 7.1 Surround", price: 600 },
+            { id: "6", name: "Webcam Full HD", price: 400 },
+            { id: "7", name: "SSD NVMe 1TB", price: 800 }
         ]);
+
     });
 });
