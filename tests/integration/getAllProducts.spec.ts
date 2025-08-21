e47b4fb0f38b9d069918623bd2aaa90c31eca922
BiancaSantana81 <biancasantana81@hotmail.com>
2025-08-21 19:31:56 -0300

    [fix] update integration test usign nock

--- tests/integration/getAllProducts.spec.ts
+++ tests/integration/getAllProducts.spec.ts
@@ -1,22 +1,27 @@
 import request from "supertest";
 import app from "../../src/index";
+import nock from "nock";
 
 describe("Products API Integration ", () => {
-
   it("deve retornar todos os produtos da API products", async () => {
-    const response = await request(app).get("/api/products");
+    nock("https://pretalab-api-439254010866.us-central1.run.app")
+      .get("/products")
+      .reply(200, {
+        data: [
+          { id: "1", name: "Notebook Gamer Pro", price: 7500 },
+          { id: "2", name: "Mouse Sem Fio Ultra-leve", price: 350 }
+        ]
+      });
+
+    const response = await request(app)
+      .get("/api/products");
 
     expect(response.status).toBe(200);
     expect(response.body).toMatchObject({
       data: [
         { id: "1", name: "Notebook Gamer Pro", price: 7500 },
         { id: "2", name: "Mouse Sem Fio Ultra-leve", price: 350 },
-        { id: "3", name: "Teclado Mecânico RGB", price: 550 },
-        { id: "4", name: "Monitor 4K 27\"", price: 2500 },
-        { id: "5", name: "Headset 7.1 Surround", price: 600 },
-        { id: "6", name: "Webcam Full HD", price: 400 },
-        { id: "7", name: "SSD NVMe 1TB", price: 800 }
-      ]
+      ],
     });
   });
 });
