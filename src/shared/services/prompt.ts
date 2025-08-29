84148a323a9984a370bde487017b1fdf83565801
BiancaSantana81 <biancasantana81@hotmail.com>
2025-08-28 22:32:25 -0300

    [feature] update financialAssitant for use database

--- src/shared/services/prompt.ts
+++ src/shared/services/prompt.ts
@@ -1,28 +1,23 @@
-import { chat, generateText } from "../adapters/gemini";
+import { getAllTransactions } from "../../modules/transactions/transaction";
+import { chat, financialAssitant, generateText } from "../adapters/gemini";
+import { ChatHistory } from "../utils/chatHistory.model";
+import { ChatRepository } from "../utils/chatHistory.repository";
 import { geminiInteral } from "../utils/gemini";
 
-const chatHistory: any = [];
-
-function buildMessage(role: string, text: string) {
-
-    chatHistory.push(
-        {
-            role,
-            parts: [{ text }],
-        }
-
-    );
-}
+const chatRepo = new ChatRepository();
+const transactionContext = () => getAllTransactions();
 
 export const handleConversation = async (prompt: string) => {
+    const transactionData = await transactionContext();
 
-    buildMessage("user", prompt);
+    await chatRepo.createMessage("user", prompt);
 
-    const data = await chat(chatHistory);
-    const { response } = geminiInteral(data);
+    const chatHistory = await chatRepo.getAllMessages("asc");
 
-    buildMessage("model", response);
+    const data = await financialAssitant(chatHistory, transactionData);
+    const { response } = await geminiInteral(data);
 
+    await chatRepo.createMessage("model", response);
     return {
         response,
         chatHistory,
